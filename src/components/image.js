7d92f49075386010e91933053bce7af06db1afa1
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2020-02-21 21:11:51 +0100

    Mobile looks fixes

--- src/components/image.js
+++ src/components/image.js
@@ -26,7 +26,7 @@ const Image = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} style={{ marginBottom: 100 }} />
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
 }
 
 export default Image
